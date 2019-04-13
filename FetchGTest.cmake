9ea0f0327d7e9f26d10ba9cecdca652075ccec36
Samuel B. Johnson <sabjonso@yahoo.com>
2019-04-12 19:48:00 -0500

    Added namespaces to gtest libraries in FetchGTest

--- FetchGTest.cmake
+++ FetchGTest.cmake
@@ -1,4 +1,5 @@
 include(FetchContent)
+include(Assert)
 
 FetchContent_Declare(googletest
   GIT_REPOSITORY https://github.com/google/googletest)
@@ -7,4 +8,11 @@ FetchContent_GetProperties(googletest)
 if(NOT googletest_POPULATED)
   FetchContent_Populate(googletest)
   add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
+endif()
+
+if(NOT TARGET google::gtest)
+  add_library(google::gtest ALIAS gtest)
+  add_library(google::gtest_main ALIAS gtest_main)
+  add_library(google::gmock ALIAS gmock)
+  add_library(google::gmock_main ALIAS gmock_main)
 endif()
