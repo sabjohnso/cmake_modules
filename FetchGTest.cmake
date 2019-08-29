33029b9da3b86894e320e06238418dd3c900e8bc
Samuel B. Johnson <sabjonso@yahoo.com>
2019-08-28 21:21:44 -0500

    Added aliases to the google test libraries

--- FetchGTest.cmake
+++ FetchGTest.cmake
@@ -8,3 +8,8 @@ if(NOT googletest_POPULATED)
   FetchContent_Populate(googletest)
   add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
 endif()
+
+add_library(GTest::gtest ALIAS gtest)
+add_library(GTest::gtest_main ALIAS gtest_main)
+add_library(GTest::gmock ALIAS gmock)
+add_library(GTest::gmock_main ALIAS gmock_main)
