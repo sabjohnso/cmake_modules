e31ffa191e6edc0b0a856c4b282f1d3dccc37178
Samuel B. Johnson <sabjonso@yahoo.com>
2019-08-30 18:05:29 -0500

    Allow the use of installed JSON

--- FetchNLohmannJSON.cmake
+++ FetchNLohmannJSON.cmake
@@ -1,12 +1,20 @@
 include(FetchContent)
 
-FetchContent_Declare(
-  nlohmann_json
-  GIT_REPOSITORY https://github.com/nlohmann/json.git)
+option( FORCE_DOWNLOAD_NLOHMANN_JSON "Download NLohmann JSON even if it is installed" OFF)
 
-FetchContent_GetProperties(nlohmann_json)
-if(NOT nlohmann_json_POPULATED)
-  FetchContent_Populate(nlohmann_json)
-  set(JSON_BuildTests OFF CACHE INTERNAL "")
-  add_subdirectory(${nlohmann_json_SOURCE_DIR} ${nlohmann_json_BINARY_DIR})
+if(NOT FORCE_DOWNLOAD_NLOHMANN_JSON)
+  find_package(nlohmann_json)
+endif()
+
+if(NOT nlohmann_json_FOUND)
+  FetchContent_Declare(
+    nlohmann_json
+    GIT_REPOSITORY https://github.com/nlohmann/json.git)
+  
+  FetchContent_GetProperties(nlohmann_json)
+  if(NOT nlohmann_json_POPULATED)
+    FetchContent_Populate(nlohmann_json)
+    set(JSON_BuildTests OFF CACHE INTERNAL "")
+    add_subdirectory(${nlohmann_json_SOURCE_DIR} ${nlohmann_json_BINARY_DIR})
+  endif()
 endif()
