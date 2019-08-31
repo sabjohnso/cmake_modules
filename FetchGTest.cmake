9a403836b9aeaeacf13f1541fd07fd07f562a3b0
Samuel B. Johnson <sabjonso@yahoo.com>
2019-08-30 19:33:48 -0500

    options to use system gtest

--- FetchGTest.cmake
+++ FetchGTest.cmake
@@ -1,17 +1,25 @@
 include(FetchContent)
-include(Assert)
 
-FetchContent_Declare(googletest
-  GIT_REPOSITORY https://github.com/google/googletest)
+option( FORCE_DOWNLOAD_GTEST "Download GTesT even if it is installed" OFF)
 
-FetchContent_GetProperties(googletest)
-if(NOT googletest_POPULATED)
-  FetchContent_Populate(googletest)
-  add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR} EXCLUDE_FROM_ALL)
+if(NOT FORCE_DOWNLOAD_GTEST)
+  find_package(GTest)
 endif()
 
-add_library(GTest::gtest ALIAS gtest)
-add_library(GTest::gtest_main ALIAS gtest_main)
-add_library(GTest::gmock ALIAS gmock)
-add_library(GTest::gmock_main ALIAS gmock_main)
+if(NOT GTest_FOUND)
+  FetchContent_Declare(googletest
+    GIT_REPOSITORY https://github.com/google/googletest)
+
+  FetchContent_GetProperties(googletest)
+  if(NOT googletest_POPULATED)
+    FetchContent_Populate(googletest)
+    add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR} EXCLUDE_FROM_ALL)
+  endif()
+
+  add_library(GTest::gtest ALIAS gtest)
+  add_library(GTest::gtest_main ALIAS gtest_main)
+  add_library(GTest::gmock ALIAS gmock)
+  add_library(GTest::gmock_main ALIAS gmock_main)
+
+endif()
 
--- FetchNLohmannJSON.cmake
+++ FetchNLohmannJSON.cmake
@@ -2,19 +2,29 @@ include(FetchContent)
 
 option( FORCE_DOWNLOAD_NLOHMANN_JSON "Download NLohmann JSON even if it is installed" OFF)
 
+
 if(NOT FORCE_DOWNLOAD_NLOHMANN_JSON)
   find_package(nlohmann_json)
 endif()
 
 if(NOT nlohmann_json_FOUND)
+  option( INTSTALL_NLOHMANN_JSON "Install NLohmann JSON" OFF )
+
+  if(INSTALL_NLOHMANN_JSON)
+    set(NLOHMANN_EXCLUDE_FLAG EXCLUDE_FROM_ALL)
+  else()
+    set(NLOHMANN_EXCLUDE_FLAG "")
+  endif()
+  
   FetchContent_Declare(
     nlohmann_json
     GIT_REPOSITORY https://github.com/nlohmann/json.git)
+  
   
   FetchContent_GetProperties(nlohmann_json)
   if(NOT nlohmann_json_POPULATED)
     FetchContent_Populate(nlohmann_json)
     set(JSON_BuildTests OFF CACHE INTERNAL "")
-    add_subdirectory(${nlohmann_json_SOURCE_DIR} ${nlohmann_json_BINARY_DIR})
+    add_subdirectory(${nlohmann_json_SOURCE_DIR} ${nlohmann_json_BINARY_DIR} ${NLOHMANN_EXCLUDE_FLAG})
   endif()
 endif()
